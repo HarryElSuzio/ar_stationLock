d812185a8fbd56486356d1d33c1f10ff873923e7
Fred Richter <45440493+Heady20006@users.noreply.github.com>
2019-04-20 13:38:13 +0200

    edit version

--- __resource.lua
+++ __resource.lua
@@ -1,4 +1,11 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-server_script 's_main.lua'
-client_script 'c_main.lua'
+server_scripts {
+	'config.lua',
+	's_main.lua'
+}
+	
+client_scripts {
+	'config.lua',
+	'c_main.lua'
+}
